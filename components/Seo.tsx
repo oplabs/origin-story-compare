8935fd802033c03b848765e026b3be8c6ec84f92
Tom Hirst <hello@tomhirst.com>
2022-12-14 10:57:26 +0000

    Add missing meta tags

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -7,20 +7,26 @@ interface SeoProps {
   description: string;
 }
 
-const Seo: FunctionComponent<SeoProps> = ({ title, ogTitle, description }) => (
-  <Head>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta
-      property="og:image"
-      content={`${
-        process.env.NEXT_PUBLIC_VERCEL_URL
-          ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
-          : ""
-      }/api/og${ogTitle ? `?title=${ogTitle}` : ""}`}
-    />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-);
+const Seo: FunctionComponent<SeoProps> = ({ title, ogTitle, description }) => {
+  const seoImage = `${
+    process.env.NEXT_PUBLIC_VERCEL_URL
+      ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
+      : ""
+  }/api/og${ogTitle ? `?title=${ogTitle}` : ""}`;
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={ogTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={seoImage} />
+      <meta property="twitter:title" content={ogTitle} />
+      <meta property="twitter:image" content={seoImage} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
 
 export { Seo };

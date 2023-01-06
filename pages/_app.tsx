2dc5f154c574712181827b744cef25c358bca295
Tom Hirst <hello@tomhirst.com>
2023-01-06 11:14:04 +0000

    Add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { GTM_ID, pageview } from "../lib/gtm";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  useEffect(() => {
+    if (!GTM_ID) return;
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {GTM_ID && (
+        <Script
+          id="gtag-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+          }}
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }

96646cad8dbd89d0cb7bb1cd16fbc3329485b2bb
Tom Hirst <hello@tomhirst.com>
2023-02-10 15:28:49 +0000

    Add Google Form to CTA

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: FunctionComponent = () => (
       heading="Want Access to This Data?"
       subheading="We're building an NFT intelligence platform for web3 developers. Join the waitlist to be the first to hear when our APIs are ready."
       buttonText="Join waitlist"
-      buttonHref="mailto:engineering@originprotocol.com?subject=NFT intelligence platform waitlist&body=Please add me to the waitlist for Origin Protocol's NFT intelligence platform."
+      buttonHref="https://docs.google.com/forms/d/e/1FAIpQLScboPL5rZGRuRqgkCknnv-od29aDdiq8ULq5dkxoPs1DOgrug/viewform"
     />
     <footer className="bg-neutral text-neutral-content py-8 text-xs text-center">
       <Wrapper>
